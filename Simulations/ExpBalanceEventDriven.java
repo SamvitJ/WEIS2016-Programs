7a3b1224b86a8a68bd555ab3b73566ee1ca165ff
SamvitJ <samvit.jain@gmail.com>
2016-05-23 15:18:04 -0700

    Added command line arguments support. Cleanup.

--- Simulations/ExpBalanceEventDriven.java
+++ Simulations/ExpBalanceEventDriven.java
@@ -42,16 +42,16 @@ public class ExpBalanceEventDriven {
    }
    
    /* Refills hot wallet if empty. Cold wallet is robbed with
-   probability pTheft after transaction. Return 1 if cold wallet
-   theft occurred, 0 if not. */
+   probability pTheft after transaction.
+   Returns true if cold wallet theft occurred, false if not. */
    private boolean refillHot(int mu, double pTheft) {
 
-      /* if cold wallet does not have enough funds to refill hot, alert */
-      /*if (this.coldBalance < mu)
-         System.out.println("************ BANKRUPTCY!!! ************ ");*/
-
-      /* Refill hot */
-      this.coldBalance -= mu;
+      /* Refill hot wallet */
+      if (this.coldBalance < mu) {
+         this.coldBalance = 0;
+      } else {
+         this.coldBalance -= mu;
+      }
       this.hotBalance = mu;
       this.coldHotTransfers++;
 
@@ -68,26 +68,53 @@ public class ExpBalanceEventDriven {
 
    public static void main(String[] args) {
 
+      if (args.length < 4 && args.length > 7) {
+         System.out.printf("Usage: java ExpBalanceEventDriven mean_d mean_w mean_t_h p_t_c [iterations] [mu_low mu_high]\n" +
+            "  e.g. java ExpBalanceEventDriven 80.0 78.0 0.01 0.01 1000 100 130\n");
+         return;
+      }
+
       r = new Random();
 
-      /* simulation parameters ************/
-      final double mD = 79;
-      final double mW = 78;
-      final double mTh = 0.01;       /* expected: 1 theft every 100 hours     */
-      final double pTc = 0.01;       /* expected: 1 theft every 100 accesses  */
+      final double mD = Double.parseDouble(args[0]);
+      final double mW = Double.parseDouble(args[1]);
+      final double mTh = Double.parseDouble(args[2]);
+      final double pTc = Double.parseDouble(args[3]);
+
+      final int iterations;
+      final int muLow;
+      final int muHigh;
+
+      if (args.length == 4) {
+         iterations = 1000;
+         muLow = 100;
+         muHigh = 130;
+      }
+      else if (args.length == 5) {
+         iterations = Integer.parseInt(args[4]);
+         muLow = 100;
+         muHigh = 130;
+      }
+      else if (args.length == 6) {
+         iterations = 1000;
+         muLow = Integer.parseInt(args[4]);
+         muHigh = Integer.parseInt(args[5]);
+      }
+      else { // if (args.length == 7) {
+         iterations = Integer.parseInt(args[4]);
+         muLow = Integer.parseInt(args[5]);
+         muHigh = Integer.parseInt(args[6]);
+      }
 
       /* in seconds */
       long timeSpan = 3600 * 20000;
-      /************************************/
 
-      System.out.println("\nmD: " + mD);
-      System.out.println("mu  exp_bal run_time");
+      System.out.println("\n mu  exp_bal");
+      System.out.println("----------------");
 
-      for (int mu = 65; mu <= 85; mu++) {
+      for (int mu = muLow; mu <= muHigh; mu++) {
 
-         /* testing parameters/stats *********/
-         int iterations = 1000;
-
+         /* Statistics *********/
          int totColdHot = 0;
          int totHotCold = 0;
 
@@ -97,16 +124,13 @@ public class ExpBalanceEventDriven {
          int totColdBal = 0;
          int totHotBal = 0;
          int totBalance = 0;
-         /************************************/
 
-         /* simulation parameters ************/
+         /* Other parameters ************/
          int startingHot = mu;
          int startingCold = 0;
-         /************************************/
 
          long t1 = System.currentTimeMillis();
 
-         /* tests */
          for (int j = 0; j < iterations; j++) {
 
             ExpBalanceEventDriven sim = new ExpBalanceEventDriven();
@@ -120,76 +144,50 @@ public class ExpBalanceEventDriven {
             sim.hotBalance = startingHot;
             sim.coldBalance = startingCold;
 
-            /* in seconds */
-            double time = 0.0;
+            double time = 0.0;      /* in seconds */
 
-            double nextD = timeToEvent(mD/3600.0);    // next deposit
-            double nextW = timeToEvent(mW/3600.0);    // next withdrawal
-            double nextT = timeToEvent(mTh/3600.0);   // next hot wallet theft
+            double timeToD = timeToEvent(mD/3600.0);    // next deposit
+            double timeToW = timeToEvent(mW/3600.0);    // next withdrawal
+            double timeToT = timeToEvent(mTh/3600.0);   // next hot wallet theft
 
             while (time < timeSpan) {
 
-               Event nextEvent = nextEvent(nextD, nextW, nextT);
+               Event nextEvent = nextEvent(timeToD, timeToW, timeToT);
 
                if (nextEvent == Event.DEPOSIT) {
 
-                  //System.out.printf("%.2f Deposit\n", time);
-
                   if (sim.hotBalance < mu) {
                      sim.hotBalance++;
-                  }
-                  else {
+                  } else {
                      sim.coldBalance++;
                      sim.hotColdTransfers++;
                   }
                   
-                  time = nextD;
-                  nextD += timeToEvent(mD/3600.0);
-               }
+                  time = timeToD;
+                  timeToD += timeToEvent(mD/3600.0);
 
-               else if (nextEvent == Event.WITHDRAWAL) {
-
-                  //System.out.printf("%.2f Withdrawal\n", time);
+               } else if (nextEvent == Event.WITHDRAWAL) {
 
                   sim.hotBalance--;
 
-                  time = nextW;
-                  nextW += timeToEvent(mW/3600.0);
-               }
+                  time = timeToW;
+                  timeToW += timeToEvent(mW/3600.0);
 
-               else {
-
-                  //System.out.printf("%.2f Hot Theft!\n", time);
+               } else {
 
                   sim.hotThefts++;
                   sim.hotBalance = 0;
 
-                  time = nextT;
-                  nextT += timeToEvent(mTh/3600.0);
+                  time = timeToT;
+                  timeToT += timeToEvent(mTh/3600.0);
                }
 
-               /* hot wallet refill */
+               /* Hot wallet refill */
                if (sim.hotBalance <= 0) {
-                  //System.out.printf("%.2f Hot Refill\n", time);
 
-                  if (sim.refillHot(mu, pTc)) {
-                     //System.out.printf("%.2f Cold Theft!\n", time);
-                  }
+                  sim.refillHot(mu, pTc);
                }
-
-               //System.out.println("Hot wallet balance: " + sim.hotBalance);
-               //System.out.println("Cold wallet balance: " + sim.coldBalance);
             }
-
-            /* System.out.println("\nStats for Trial " + j + ":");
-            System.out.println("Hot -> cold transfers: " + sim.hotColdTransfers);
-            System.out.println("Cold -> hot transfers: " + sim.coldHotTransfers);
-
-            System.out.println("Hot w. thefts: " + sim.hotThefts);
-            System.out.println("Cold w. thefts: " + sim.coldThefts);
-
-            System.out.println("Balance: " + (sim.hotBalance - startingHot + sim.coldBalance - startingCold));
-            System.out.println(); */
 
             /* Record cumulative statistics */
             totHotCold += sim.hotColdTransfers;
@@ -204,22 +202,19 @@ public class ExpBalanceEventDriven {
             totBalance += sim.hotBalance + sim.coldBalance;
          }
 
-         long t2 = System.currentTimeMillis();
-         double timeElapsed = (t2 - t1)/1000.0; 
-
-         System.out.println(mu + "  " + (double)totBalance/(double)iterations + " " + timeElapsed);
-
-         //System.out.println("\nOTHER CUMULATIVE STATISTICS ******************\n");
+         /* Print stats for current value of mu */
+         System.out.printf("%d  %5.1f\n", mu, (double)totBalance/(double)iterations);
          
+         /* Other cumulative statistics
          System.out.println("Avg h->c transfers: " + (double)totHotCold/(double)iterations);
          System.out.println("Avg c->h transfers: " + (double)totColdHot/(double)iterations);
 
-         System.out.println("Avg h. w. thefts: " + (double)totHotThefts/(double)iterations);
-         System.out.println("Avg c. w. thefts: " + (double)totColdThefts/(double)iterations);
+         System.out.println("Avg h.w. thefts: " + (double)totHotThefts/(double)iterations);
+         System.out.println("Avg c.w. thefts: " + (double)totColdThefts/(double)iterations);
 
-         System.out.println("Avg h. w. balance: " + (double)totHotBal/(double)iterations);
-         System.out.println("Avg c. w. balance: " + (double)totColdBal/(double)iterations);
-         System.out.println();
+         System.out.println("Avg h.w. balance: " + (double)totHotBal/(double)iterations);
+         System.out.println("Avg c.w. balance: " + (double)totColdBal/(double)iterations);
+         System.out.println(); */
       }
 
    }
